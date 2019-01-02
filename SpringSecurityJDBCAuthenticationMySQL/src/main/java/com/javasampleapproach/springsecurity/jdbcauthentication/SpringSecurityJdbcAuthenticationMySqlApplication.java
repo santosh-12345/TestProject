e7a7930a0a3a4fd080eb542c1fc6760306ebc1f1
S.N Reddy <S.N Reddy@192.168.0.122>
2019-01-02 14:10:43 +0530

    Spring Security JDBC Auth MySQL

---
+++ SpringSecurityJDBCAuthenticationMySQL/src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/SpringSecurityJdbcAuthenticationMySqlApplication.java
@@ -0,0 +1,14 @@
+package com.javasampleapproach.springsecurity.jdbcauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcAuthenticationMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbcAuthenticationMySqlApplication.class, args);
+	}
+}
+
+// USED url: https://grokonez.com/spring-framework/spring-security/use-spring-security-jdbc-authentication-mysql-spring-boot
---
+++ SpringSecurityJDBCAuthenticationMySQL/src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.javasampleapproach.springsecurity.jdbcauthentication.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+ 
+@Configuration
+@EnableAutoConfiguration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+	@Autowired
+	DataSource dataSource;
+ 
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username,password, enabled from userssss where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+	}
+ 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().antMatchers("/admin").hasRole("ADMIN")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll();
+		http.exceptionHandling().accessDeniedPage("/403");
+	}
+}
+
+// Used URL: https://docs.spring.io/autorepo/docs/spring-security-javaconfig/1.0.0.CI-SNAPSHOT/api-reference/org/springframework/security/config/annotation/provisioning/JdbcUserDetailsManagerConfigurator.html
+// usersByUsernameQuery(java.lang.String query) ---- Sets the query to be used for finding a user by their username.
+// authoritiesByUsernameQuery(java.lang.String query) ---- Sets the query to be used for finding a user's authorities by their username.
